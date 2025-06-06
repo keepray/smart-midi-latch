371352c0464188240dfcaca182b8907096fcec97
keepray <113338967+keepray@users.noreply.github.com>
2025-06-06 16:00:32 +0200

    Update latch-script.js
    Version 2.0

--- latch-script.js
+++ latch-script.js
@@ -1,10 +1,10 @@
 // MIDI-Latch script by Felix Gabel
-// Version 1.0
+// Verson 2.0
 
-var physicalNotesCurrentlyHeldDown = []
+var physicalNotesCurrentlyHeldDown = new Set()
 var physicalPedalIsHeldDown = false
-var notesCurrentlySustainedWithPedal = []
-var currentlyLatchedNotes = []
+var notesCurrentlySustainedWithPedal = new Set()
+var currentlyLatchedNotes = new Set()
 
 var PluginParameters = [
     {
@@ -17,97 +17,85 @@ var PluginParameters = [
 ]
 
 function HandleMIDI(event) {
-    var mode = GetParameter("Latch")
+    const mode = GetParameter("Latch")
 
-    // note on
+    // Note On
     if (event instanceof NoteOn) {
-        if (mode === 0 && !notesCurrentlySustainedWithPedal.includes(event.pitch)) { // Remove " && !notesCurrentlySustainedWithPedal.includes(event.pitch)" to allow retriggering notes held by the pedal. But I recommend keeping it if you’re using a pad sound, because it usually sounds better that way.
+        if (mode === 0 && !notesCurrentlySustainedWithPedal.has(event.pitch)) { // Remove " && !notesCurrentlySustainedWithPedal.has(event.pitch)" to allow retriggering notes held by the pedal. But I recommend keeping it if you’re using a pad sound, because it usually sounds better that way.
             event.send()
         }
 
-        if (!physicalNotesCurrentlyHeldDown.includes(event.pitch)) {
-            physicalNotesCurrentlyHeldDown.push(event.pitch)
-        }
+        physicalNotesCurrentlyHeldDown.add(event.pitch)
 
-        if (physicalPedalIsHeldDown && !notesCurrentlySustainedWithPedal.includes(event.pitch)) {
-            notesCurrentlySustainedWithPedal.push(event.pitch)
+        if (physicalPedalIsHeldDown) {
+            notesCurrentlySustainedWithPedal.add(event.pitch)
         }
 
         return
     }
 
-    // note off
+    // Note Off
     if (event instanceof NoteOff) {
-        if (mode === 0 && (!physicalPedalIsHeldDown || !notesCurrentlySustainedWithPedal.includes(event.pitch))) {
-        		event.send()
-    		}
-
-        var index = physicalNotesCurrentlyHeldDown.indexOf(event.pitch)
-        if (index !== -1) {
-            physicalNotesCurrentlyHeldDown.splice(index, 1)
+        if (mode === 0 && (!physicalPedalIsHeldDown || !notesCurrentlySustainedWithPedal.has(event.pitch))) {
+            event.send()
         }
 
+        physicalNotesCurrentlyHeldDown.delete(event.pitch)
+        
         return
     }
 
-    // sustain pedal on/off
+    // Sustain Pedal On/Off
     if (event instanceof ControlChange && event.number === 64) {
         physicalPedalIsHeldDown = event.value >= 64
 
-		if (event.value >= 64) { // pedal was pressed
-        		for (var i = 0; i < physicalNotesCurrentlyHeldDown.length; i++) {
-            		var pitch = physicalNotesCurrentlyHeldDown[i]
-            		if (!notesCurrentlySustainedWithPedal.includes(pitch)) {
-                		notesCurrentlySustainedWithPedal.push(pitch)
-            		}
-        		}
-    		}
-
-        if (event.value < 64) { // pedal was released
+        if (physicalPedalIsHeldDown) {
+            for (const pitch of physicalNotesCurrentlyHeldDown) {
+                notesCurrentlySustainedWithPedal.add(pitch)
+            }
+        } else {
             if (mode === 0) {
-                for (var i = 0; i < notesCurrentlySustainedWithPedal.length; i++) {
-                    var pitch = notesCurrentlySustainedWithPedal[i]
-                    if (!physicalNotesCurrentlyHeldDown.includes(pitch)) {
-                        var noteOff = new NoteOff()
+                for (const pitch of notesCurrentlySustainedWithPedal) {
+                    if (!physicalNotesCurrentlyHeldDown.has(pitch)) {
+                        const noteOff = new NoteOff()
                         noteOff.pitch = pitch
                         noteOff.send()
                     }
                 }
             }
-
-            notesCurrentlySustainedWithPedal = []
+            
+            notesCurrentlySustainedWithPedal.clear()
         }
-
+        
         return
     }
 
-    // sending all remaining events
+    // Sending all remaining events
     event.send()
 }
 
 function ParameterChanged(param) {
-    var mode = GetParameter("Latch")
+    const mode = GetParameter("Latch")
 
-    // latch ON
+    // Latch ON
     if (mode === 1) {
-        currentlyLatchedNotes = physicalPedalIsHeldDown ? notesCurrentlySustainedWithPedal.slice() : physicalNotesCurrentlyHeldDown.slice()
+        currentlyLatchedNotes = physicalPedalIsHeldDown ? new Set(notesCurrentlySustainedWithPedal) : new Set(physicalNotesCurrentlyHeldDown)
     }
 
-    // latch OFF
+    // Latch OFF
     if (mode === 0) {
-    		if (physicalPedalIsHeldDown) {
-        		notesCurrentlySustainedWithPedal = currentlyLatchedNotes.slice()
-    		}
-    
-        for (var i = 0; i < currentlyLatchedNotes.length; i++) {
-            var pitch = currentlyLatchedNotes[i]
-            if (!physicalNotesCurrentlyHeldDown.includes(pitch) && !notesCurrentlySustainedWithPedal.includes(pitch)) {
-                var noteOff = new NoteOff()
+        if (physicalPedalIsHeldDown) {
+            notesCurrentlySustainedWithPedal = new Set(currentlyLatchedNotes)
+        }
+        
+        for (const pitch of currentlyLatchedNotes) {
+            if (!physicalNotesCurrentlyHeldDown.has(pitch) && !notesCurrentlySustainedWithPedal.has(pitch)) {
+                const noteOff = new NoteOff()
                 noteOff.pitch = pitch
                 noteOff.send()
             }
         }
         
-        currentlyLatchedNotes = []
+        currentlyLatchedNotes.clear()
     }
 }
